69dcca4dc8d854175e0668e113b94ef5a1b3df88
Ben Toon <bentoon00+github@gmail.com>
2026-04-24 12:15:49 +0100

    Fix ARM toolchain configuration for macOS
    Added support for official ARM GNU Embedded Toolchain path on macOS and adjusted find_program calls to prioritize this path. Updated CMAKE_OSX_SYSROOT settings for ARM cross-compilation.

--- utils/cmake/toolchains/ARM_GCC/toolchain.cmake
+++ utils/cmake/toolchains/ARM_GCC/toolchain.cmake
@@ -1,15 +1,34 @@
-find_program(ARM_NONE_EABI_RANLIB arm-none-eabi-ranlib)
-find_program(ARM_NONE_EABI_AR arm-none-eabi-ar)
-find_program(ARM_NONE_EABI_GCC arm-none-eabi-gcc)
-find_program(ARM_NONE_EABI_GPP arm-none-eabi-g++)
-find_program(ARM_NONE_EABI_OBJCOPY arm-none-eabi-objcopy)
-find_program(ARM_NONE_EABI_SIZE arm-none-eabi-size)
+# Official ARM GNU Embedded Toolchain path (macOS)
+set(ARM_TOOLCHAIN_PATH "/Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi")
 
-set(CMAKE_OSX_SYSROOT "/")
+# Look for toolchain binaries in the official installation first, then system PATH
+find_program(ARM_NONE_EABI_RANLIB arm-none-eabi-ranlib PATHS "${ARM_TOOLCHAIN_PATH}/bin" NO_DEFAULT_PATH)
+find_program(ARM_NONE_EABI_AR arm-none-eabi-ar PATHS "${ARM_TOOLCHAIN_PATH}/bin" NO_DEFAULT_PATH)
+find_program(ARM_NONE_EABI_GCC arm-none-eabi-gcc PATHS "${ARM_TOOLCHAIN_PATH}/bin" NO_DEFAULT_PATH)
+find_program(ARM_NONE_EABI_GPP arm-none-eabi-g++ PATHS "${ARM_TOOLCHAIN_PATH}/bin" NO_DEFAULT_PATH)
+find_program(ARM_NONE_EABI_OBJCOPY arm-none-eabi-objcopy PATHS "${ARM_TOOLCHAIN_PATH}/bin" NO_DEFAULT_PATH)
+find_program(ARM_NONE_EABI_SIZE arm-none-eabi-size PATHS "${ARM_TOOLCHAIN_PATH}/bin" NO_DEFAULT_PATH)
+
+# If not found in the official path, try system PATH
+if(NOT ARM_NONE_EABI_GCC)
+    find_program(ARM_NONE_EABI_RANLIB arm-none-eabi-ranlib)
+    find_program(ARM_NONE_EABI_AR arm-none-eabi-ar)
+    find_program(ARM_NONE_EABI_GCC arm-none-eabi-gcc)
+    find_program(ARM_NONE_EABI_GPP arm-none-eabi-g++)
+    find_program(ARM_NONE_EABI_OBJCOPY arm-none-eabi-objcopy)
+    find_program(ARM_NONE_EABI_SIZE arm-none-eabi-size)
+endif()
+
+# For cross-compilation on macOS, disable macOS-specific settings
+# CMAKE_OSX_SYSROOT should not be set for ARM cross-compilation
+set(CMAKE_OSX_SYSROOT "")
 set(CMAKE_OSX_DEPLOYMENT_TARGET "")
 
 set(CMAKE_SYSTEM_NAME "Generic")
 set(CMAKE_SYSTEM_VERSION "2.0.0")
+
+# Set sysroot for cross-compilation (empty for ARM bare-metal)
+set(CMAKE_SYSROOT "")
 
 set(CODAL_TOOLCHAIN "ARM_GCC")
 
